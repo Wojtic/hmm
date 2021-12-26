1cc8e5d8587384f2d2a553cb116c8e56ceb13bd4
Wojtic <vojtacerny012@gmail.com>
2021-12-26 15:17:30 +0100

    Added MySQL and error dialog

--- hmmlib/hmm.py
+++ hmmlib/hmm.py
@@ -1,23 +1,49 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+import mysql.connector
 
 
 class HMM(Gtk.Window):
+
     def __init__(self):
         super().__init__(title="HMM")
+        self.connection_data = None
+        self.connection = None
         self.button = Gtk.Button(label="Connection settings")
         self.button.connect("clicked", self.show_connection_dialog)
         self.add(self.button)
 
+    def connect_to_database(self):
+        try:
+            self.connection = mysql.connector.connect(**self.connection_data)
+            return self.connection
+        except mysql.connector.Error as err:
+            print(err)
+            self.show_error_dialog(self, err.msg)
+            return None
+
+    def show_error_dialog(self, widget, message):
+        dialog = Gtk.Dialog(title="Error",
+                            parent=self,
+                            modal=True)
+        dialog.add_button(Gtk.STOCK_CLOSE, Gtk.ResponseType.CLOSE)
+        dialog.set_default_size(150, 100)
+        dialog.get_content_area().add(Gtk.Label(label=message))
+
+        dialog.show_all()
+        dialog.run()
+        dialog.destroy()
+
     def show_connection_dialog(self, widget):
         dialog = Gtk.Dialog(title="Connect to server",
                             parent=self,
-                            flags=Gtk.DialogFlags.MODAL,
-                            buttons=["Connect", Gtk.ResponseType.OK])
+                            modal=True)
+        dialog.add_buttons("Cancel", Gtk.ResponseType.CANCEL,
+                           "Connect", Gtk.ResponseType.OK)
         dialog.set_default_size(150, 100)
 
-        a_label = Gtk.Label("Server Adress:")
+        a_label = Gtk.Label(label="Server Adress:")
         a_label.set_halign(Gtk.Align.START)
         dialog.vbox.pack_start(a_label, True, True, 0)
 
@@ -26,7 +52,7 @@ class HMM(Gtk.Window):
         a_entry.set_hexpand(True)
         dialog.vbox.pack_start(a_entry, True, True, 0)
 
-        p_label = Gtk.Label("Server Port:")
+        p_label = Gtk.Label(label="Server Port:")
         p_label.set_halign(Gtk.Align.START)
         dialog.vbox.pack_start(p_label, True, True, 0)
 
@@ -35,7 +61,7 @@ class HMM(Gtk.Window):
         p_entry.set_hexpand(True)
         dialog.vbox.pack_start(p_entry, True, True, 0)
 
-        un_label = Gtk.Label("Username:")
+        un_label = Gtk.Label(label="Username:")
         un_label.set_halign(Gtk.Align.START)
         dialog.vbox.pack_start(un_label, True, True, 0)
 
@@ -44,7 +70,7 @@ class HMM(Gtk.Window):
         un_entry.set_hexpand(True)
         dialog.vbox.pack_start(un_entry, True, True, 0)
 
-        pw_label = Gtk.Label("Password:")
+        pw_label = Gtk.Label(label="Password:")
         pw_label.set_halign(Gtk.Align.START)
         dialog.vbox.pack_start(pw_label, True, True, 0)
 
@@ -53,13 +79,24 @@ class HMM(Gtk.Window):
         pw_entry.set_hexpand(True)
         dialog.vbox.pack_start(pw_entry, True, True, 0)
 
+        db_label = Gtk.Label(label="Database:")
+        db_label.set_halign(Gtk.Align.START)
+        dialog.vbox.pack_start(db_label, True, True, 0)
+
+        db_entry = Gtk.Entry()
+        db_entry.set_text("")
+        db_entry.set_hexpand(True)
+        dialog.vbox.pack_start(db_entry, True, True, 0)
+
         dialog.show_all()
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
-            print("Adress:", a_entry.get_text())
-            print("Port:", p_entry.get_value_as_int())
-            print("Username:", un_entry.get_text())
-            print("Password:", pw_entry.get_text())
+            self.connection_data = {"host": a_entry.get_text(),
+                                    "port": p_entry.get_value_as_int(),
+                                    "user": un_entry.get_text(),
+                                    "database": db_entry.get_text(),
+                                    "password": pw_entry.get_text()}
+            self.connect_to_database()
         dialog.destroy()
 
 
